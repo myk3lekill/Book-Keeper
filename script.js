b20eac56b0d04fdc220aa9227a4b35126df4eaf3
me <me@gmail.com>
2023-09-19 11:55:30 +0200

    third commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('javascript is connected')
+//DOM
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-modal');
+const webisteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+//Show Modal, Focus, Input
+function showModal() {
+    console.log('shownmodal')
+    modal.classList.add('show-modal');
+    webisteNameEl.focus();
+}
+
+//Modal Event Listeners
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false))
